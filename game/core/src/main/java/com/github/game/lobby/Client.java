4c49348f54df7c25611a48bfd3322f45771fab93
Chu Ngọc Thắng <chuthanglsz@gmail.com>
2025-10-26 09:45:04 +0700

    Create client and packages for the game lobby feature

---
+++ game/core/src/main/java/com/github/game/lobby/Client.java
@@ -0,0 +1,43 @@
+package com.github.game.lobby;
+
+import java.io.Serializable;
+
+public class Client implements Serializable {
+    private static final long serialVersionUID = 2L;
+    private int id;
+    private String name;
+    private String ip;
+    private boolean isReady;
+
+    public Client(int id, String name, String ip) {
+        this.id = id;
+        this.name = name;
+        this.ip = ip;
+        this.isReady = false; // Luôn là chưa sẵn sàng khi mới tạo
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public boolean isReady() {
+        return isReady;
+    }
+
+    public void setReady(boolean ready) {
+        isReady = ready;
+    }
+    @Override
+    public String toString() {
+        String status = isReady ? "San sang" : "Chua san sang";
+        return String.format("[ID: %d] %s (%s) - %s", id, name, ip, status);
+    }
+}
